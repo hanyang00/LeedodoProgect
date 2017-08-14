a66dc845f7bb959b2cff9cdb3af6bcb9f1d15fe4
hanyang00 <869819364@qq.com>
2017-08-14 19:47:45 +0800

    主分支更新最新功能

--- app/src/main/java/com/example/aensun/leedodoprogect/view/activity/RebatesThatActivity.java
+++ app/src/main/java/com/example/aensun/leedodoprogect/view/activity/RebatesThatActivity.java
@@ -1,10 +1,28 @@
 package com.example.aensun.leedodoprogect.view.activity;
 
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.example.aensun.leedodoprogect.R;
+
+import butterknife.Bind;
+import butterknife.OnClick;
 
 public class RebatesThatActivity extends BaseActivity {
 
 
+    @Bind(R.id.close)
+    ImageView close;
+    @Bind(R.id.particulars_issue)
+    TextView particularsIssue;
+    @Bind(R.id.particulars_change_the_date)
+    TextView particularsChangeTheDate;
+    @Bind(R.id.particulars_existing)
+    TextView particularsExisting;
+    @Bind(R.id.particulars_available_amount)
+    TextView particularsAvailableAmount;
+    @Bind(R.id.slogan)
+    TextView slogan;
 
     @Override
     protected int setContentViews() {
@@ -14,5 +32,12 @@ public class RebatesThatActivity extends BaseActivity {
     @Override
     protected void initData() {
 
+    }
+
+
+
+    @OnClick(R.id.close)
+    public void onViewClicked() {
+        finish();
     }
 }
