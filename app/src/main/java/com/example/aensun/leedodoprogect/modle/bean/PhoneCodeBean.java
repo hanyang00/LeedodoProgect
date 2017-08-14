1013dc0521b248309abbe6de0596a2bfa8d40d38
hanyang00 <869819364@qq.com>
2017-08-14 11:11:22 +0800

    注册模块完成

---
+++ app/src/main/java/com/example/aensun/leedodoprogect/modle/bean/PhoneCodeBean.java
@@ -0,0 +1,43 @@
+package com.example.aensun.leedodoprogect.modle.bean;
+
+/**
+ * Created by aensun on 2017-08-13.
+ */
+
+public class PhoneCodeBean {
+
+
+    /**
+     * code : 1000
+     * descirption : 系统处理成功
+     * object :
+     */
+
+    private String code;
+    private String descirption;
+    private String object;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescirption() {
+        return descirption;
+    }
+
+    public void setDescirption(String descirption) {
+        this.descirption = descirption;
+    }
+
+    public String getObject() {
+        return object;
+    }
+
+    public void setObject(String object) {
+        this.object = object;
+    }
+}
---
+++ app/src/main/java/com/example/aensun/leedodoprogect/modle/net/ConstantPool.java
@@ -0,0 +1,9 @@
+package com.example.aensun.leedodoprogect.modle.net;
+
+/**
+ * Created by aensun on 2017-08-13.
+ */
+
+public class ConstantPool {
+    public static final String BASE_URL = "http://123.57.33.185:8088/";
+}
