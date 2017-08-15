4609eb1b8efb5442dc7a8aee16644dd01539b3f9
shangendong <648032028@qq.com>
2017-08-15 11:47:52 +0800

    新增一键回到顶部功能，

---
+++ app/src/main/java/com/example/aensun/leedodoprogect/view/activity/AerbyItemBaiDuMessageActivity.java
@@ -0,0 +1,16 @@
+package com.example.aensun.leedodoprogect.view.activity;
+
+import com.example.aensun.leedodoprogect.R;
+
+public class AerbyItemBaiDuMessageActivity extends BaseActivity {
+
+    @Override
+    protected int setContentViews() {
+        return R.layout.activity_aerby_item_bai_du_message;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
