5439c086b4b409fa72a36947baa69c3a2c7733f5
shangendong <648032028@qq.com>
2017-08-15 11:47:52 +0800

    新增一键回到顶部功能，

---
+++ app/src/main/java/com/example/aensun/leedodoprogect/utils/ScrollViewUtils.java
@@ -0,0 +1,100 @@
+package com.example.aensun.leedodoprogect.utils;
+
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * 类描述
+ * <p>
+ * 创建人:商恩东
+ * <p>
+ * 创建时间 $date$
+ */
+
+public class ScrollViewUtils {
+
+    private int scrollY = 0;// 标记上次滑动位置
+    private View contentView;
+    public    void  getscrollview(final ScrollView scrollView, final ImageView toTopBtn){
+        if (contentView == null) {
+            contentView = scrollView.getChildAt(0);
+        }
+        /******************** 监听ScrollView滑动停止 *****************************/
+        scrollView.setOnTouchListener(new View.OnTouchListener() {
+            private int lastY = 0;
+            private int touchEventId = -9983761;
+            Handler handler = new Handler() {
+                @Override
+                public void handleMessage(Message msg) {
+                    super.handleMessage(msg);
+                    View scroller = (View) msg.obj;
+                    if (msg.what == touchEventId) {
+                        if (lastY == scroller.getScrollY()) {
+                            handleStop(scroller);
+                        } else {
+                            handler.sendMessageDelayed(handler.obtainMessage(
+                                    touchEventId, scroller), 5);
+                            lastY = scroller.getScrollY();
+                        }
+                    }
+                }
+            };
+
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    handler.sendMessageDelayed(
+                            handler.obtainMessage(touchEventId, v), 5);
+                }
+                return false;
+            }
+
+            /**
+             * ScrollView 停止
+             *
+             * @param view
+             */
+            private void handleStop(Object view) {
+                ScrollView scroller = (ScrollView) view;
+                scrollY = scroller.getScrollY();
+                doOnBorderListener(scrollView,toTopBtn);
+            }
+        });
+    }
+    /**
+     * ScrollView 的顶部，底部判断：
+     * http://blog.csdn.net/qq_21376985
+     * <p/>
+     * 其中getChildAt表示得到ScrollView的child View， 因为ScrollView只允许一个child
+     * view，所以contentView.getMeasuredHeight()表示得到子View的高度,
+     * getScrollY()表示得到y轴的滚动距离，getHeight()为scrollView的高度。
+     * 当getScrollY()达到最大时加上scrollView的高度就的就等于它内容的高度了啊~
+     *
+     * @param
+     */
+    private void doOnBorderListener(ScrollView scrollView,ImageView toTopBtn) {
+        // 底部判断
+        if (contentView != null
+                && contentView.getMeasuredHeight() <= scrollView.getScrollY()
+                + scrollView.getHeight()) {
+            toTopBtn.setVisibility(View.VISIBLE);
+            Log.i(TAG, "bottom");
+        }
+        // 顶部判断
+        else if (scrollView.getScrollY() == 0) {
+
+            Log.i(TAG, "top");
+        } else if (scrollView.getScrollY() > 30) {
+            toTopBtn.setVisibility(View.VISIBLE);
+            Log.i(TAG, "test");
+        }
+
+    }
+
+}
